1880bdeaf5311d7e889349c2f137e7e9fcee13a0
Babigdk <Babigdk@gmail.com>
2022-07-10 07:25:19 -0700

    main.h filed

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct structprint - structure content
+ * @q: the location and method to translate data to character
+ * @u: print function for specific type
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
+int _putchar(char ch);
+int _printf(char *format, ...);
