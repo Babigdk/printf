22344594f3de13ff4123a528e14df36184fb123f
Babigdk <Babigdk@gmail.com>
2022-07-10 15:55:54 -0700

    files

--- main.h
+++ main.h
@@ -20,5 +20,17 @@ typedef struct structprint
 
 int _putchar(char ch);
 int _printf(char *format, ...);
-
+int _puts(char *string);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list);
+int _abs(int number);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
