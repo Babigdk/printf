bb5f4bd7d343b5c2689e595bdbc6fd6d518c4401
Babigdk <Babigdk@gmail.com>
2022-07-10 07:39:58 -0700

    main.h filed

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct structprint - structure content
  * @q: the location and method to translate data to character
