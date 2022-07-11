52f7a564ed7b87cfbe864c3377d22b915c807f21
Babigdk <Babigdk@gmail.com>
2022-07-11 01:42:26 -0700

    files

--- _printf.c
+++ _printf.c
@@ -1,50 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _printf - prints formatted data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
+ * _printf - function my printf
+ * @format: string whit format to print
+ *
+ * Return: number of chars that print
  */
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
-	int written = 0, (*structype)(char *, va_list);
-	char q[3];
-	va_list pa;
+	va_list args;
+	int length = 0;
 
 	if (format == NULL)
 		return (-1);
-	q[2] = '\0';
-	va_start(pa, format);
-	_putchar(-1);
-	while (format[0])
-	{
-		if (format[0] == '%')
-		{
-			structype = driver(format);
-			if (structype)
-			{
-				q[0] = '%';
-				q[1] = format[1];
-				written += structype(q, pa);
-			}
-			else if (format[1] != '\0')
-			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
-			}
-			else
-			{
-				written += _putchar('%');
-				break;
-			}
-			format += 2;
-		}
-		else
-		{
-			written += _putchar(format[0]);
-			format++;
-		}
-	}
-	_putchar(-2);
-	return (written);
+
+	va_start(args, format);
+
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
